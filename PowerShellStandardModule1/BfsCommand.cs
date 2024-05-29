3d8718f786fd598c6053b5b01f12e94cc5ac4c04
sn <86227911+tbui17@users.noreply.github.com>
2024-05-29 11:54:35 -0600

    fix directory bug

--- PowerShellStandardModule1/BfsCommand.cs
+++ PowerShellStandardModule1/BfsCommand.cs
@@ -20,26 +20,38 @@ namespace PowerShellStandardModule1
         )]
         public required string Pattern;
 
+        private string? _startingDirectory;
+
         [Parameter(
             Position = 1,
             ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true,
             HelpMessage = "The directory to start the search from. Defaults to the current directory."
         )]
-        public readonly string StartingDirectory = Directory.GetCurrentDirectory();
+        public string StartingDirectory
+        {
+            get => _startingDirectory ?? CurrentProviderLocation("FileSystem").ProviderPath;
+            set => _startingDirectory = value;
+        }
 
         [Parameter(HelpMessage = "Whether or not to ignore casing when comparing names. True by default.")]
         public bool IgnoreCase = true;
 
-        [Parameter(HelpMessage = "How many results to take before stopping. Defaults to 1. Negative numbers are rounded to 0.")]
+        [Parameter(
+            HelpMessage = "How many results to take before stopping. Defaults to 1. Negative numbers are rounded to 0."
+        )]
         public int First = 1;
 
-        [Parameter(HelpMessage =
-            "How long to continue search before timing out in ms, if not all directories have been exhausted. Defaults to int32 max. Negative numbers are rounded to 0.")]
+        [Parameter(
+            HelpMessage =
+                "How long to continue search before timing out in ms, if not all directories have been exhausted. Defaults to int32 max. Negative numbers are rounded to 0."
+        )]
         public int Timeout = int.MaxValue;
 
-        [Parameter(HelpMessage =
-            "The maximum amount of items to process, regardless if they match the pattern. Defaults to int32 max. Negative numbers rounded to 0.")]
+        [Parameter(
+            HelpMessage =
+                "The maximum amount of items to process, regardless if they match the pattern. Defaults to int32 max. Negative numbers rounded to 0."
+        )]
         public int Limit = int.MaxValue;
 
         private CancellationTokenSource _cts = null!;
@@ -52,20 +64,19 @@ namespace PowerShellStandardModule1
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            
+
             IEnumerable<PSObject> result;
-            var runner = new BfsRunner(Pattern, StartingDirectory, IgnoreCase, itemsToReturn: First, limit:Limit);
+            var runner = new BfsRunner(Pattern, StartingDirectory, IgnoreCase, itemsToReturn: First, limit: Limit);
             _cts.CancelAfter(Math.Max(0, Timeout));
 
 
-            
             try
             {
                 // run task in a new thread then serialize result to PSObject
                 var task = Task.Run(() => runner.Run(_cts.Token), _cts.Token);
                 task.Wait();
                 IEnumerable<DirectoryInfo> taskResult = task.GetAwaiter().GetResult();
-                
+
                 result = taskResult.Select(x => new PSObject(x));
             }
             catch (DirectoryNotFoundException e)
@@ -83,7 +94,9 @@ namespace PowerShellStandardModule1
                         pattern = Pattern,
                         ignoreCase = IgnoreCase,
                         first = First,
-                    }, "BfsCommand.OperationCancelled");
+                    },
+                    "BfsCommand.OperationCancelled"
+                );
                 WriteInformation(record);
                 return;
             }
